9bf059415d41be2e174237d7fa6ac01761050e12
heuzin <matheusqs.arq@gmail.com>
2020-12-02 15:41:53 -0500

    Removing a color

--- src/components/Color.js
+++ src/components/Color.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FaTrash } from 'react-icons/fa';
 import StartRating from './starRating/StarRating';
 
-export default function Color({ title, color, rating }) {
+export default function Color({ id, title, color, rating, onRemove }) {
     return (
         <section>
             <h1>{title}</h1>
+            <button onClick={() => onRemove(id)}>
+                <FaTrash />
+            </button>
             <div style={{ height: 50, backgroundColor: color }}></div>
             <StartRating selectedStars={rating}/>
         </section>
